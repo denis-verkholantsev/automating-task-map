f03324a7a2360a55a2911dc69d5228103c3b6c30
dverholancev <dverholancev@webim.ru>
2025-04-22 20:22:12 +0300

    fix set boundary_path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     clipper = RasterClipper(
         source_path="/home/dverholancev/study/degree/src/20230608_F14_Micasense_NDVI.tif",
         boundary_path="/home/dverholancev/study/degree/src/boundary_field_14.shp",
-        output_path="clipped.tif",
+        output_path="clippedd.tif",
         encoding='latin1',
         preprocess_geometry=lines_to_polygon
     )
--- raster_clipper.py
+++ raster_clipper.py
@@ -3,6 +3,7 @@ import rasterio
 from rasterio.mask import mask
 from shapely.geometry import mapping
 from typing import Callable
+import numpy as np
 
 
 class RasterClipper:
@@ -22,7 +23,7 @@ class RasterClipper:
 
     def _load_geometry(self, preprocess_geometry):
         if preprocess_geometry:
-            preprocess_geometry(self.boundary_path, self.encoding)
+            self.boundary_path = preprocess_geometry(self.boundary_path, self.encoding)
 
         with rasterio.open(self.source_path) as src:
             raster_crs = src.crs
@@ -33,17 +34,26 @@ class RasterClipper:
             gdf = gdf.to_crs(raster_crs)
 
         return [mapping(geom) for geom in gdf.geometry if not geom.is_empty]
+        #return [geom.__geo_interface__ for geom in gdf.geometry.values]
 
     def clip(self):
         with rasterio.open(self.source_path) as src:
             out_image, out_transform = mask(src, self.geometry, crop=True)
             out_meta = src.meta.copy()
+            raster_crs = src.crs
+        
+        out_image = np.where(out_image == -32767.0, 0, out_image)
+        out_meta["nodata"] = 0
+        
+        print(out_image.shape) 
+        print(out_image.min(), out_image.max())
 
         out_meta.update({
             "driver": "GTiff",
             "height": out_image.shape[1],
             "width": out_image.shape[2],
-            "transform": out_transform
+            "transform": out_transform,
+            "crs": raster_crs
         })
 
         with rasterio.open(self.output_path, "w", **out_meta) as dest:
